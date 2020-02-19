6b5a5bef546cae647cdb2e71fdeb0df530a59a65
marvosyntactical <koss@cl.uni-heidelberg.de>
2020-02-19 15:56:33 +0100

    no changes

--- data/scripts/parse_kvr_json.py
+++ data/scripts/parse_kvr_json.py
@@ -2,29 +2,15 @@ import json
 import os
 import sys
 
-def read_json(filename):
-    contents = json.load(filename)
-    return contents
+
+
 
 def main(args):
     directory = "../kv_ret_dataset/"
     filename = "kvret_entities.json" if args == 0 else args[0]
     with open(directory+filename, "r") as f:
-        dicc = read_json(f)
-    if type(dicc) == list:
-
-
-
-
-        print(len(dicc[0]))
-        print(dicc[0].keys())
-        print(dicc[0]["scenario"].keys())
-
-
-
-    else:
-        print(len(dicc))
-        print(dicc.keys())
+        dicc = json.load(f)
+    
 
 
     return 0
