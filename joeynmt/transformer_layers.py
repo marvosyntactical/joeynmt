4c1648bf7c5d2bbabfe4d38cd4ab05227de241a6
marvosyntactical <koss@cl.uni-heidelberg.de>
2020-08-26 14:39:11 +0200

    notes

--- joeynmt/transformer_layers.py
+++ joeynmt/transformer_layers.py
@@ -192,10 +192,11 @@ class PositionalEncoding(nn.Module):
         if size % 2 != 0:
             raise ValueError("Cannot use sin/cos positional encoding with "
                              "odd dim (got dim={:d})".format(size))
-        pe = torch.zeros(max_len, size)
+        pe = torch.zeros( max_len, size )
         position = torch.arange(0, max_len).unsqueeze(1)
-        div_term = torch.exp((torch.arange(0, size, 2, dtype=torch.float) *
-                              -(math.log(10000.0) / size)))
+        div_term = torch.exp(
+            (torch.arange(0, size, 2, dtype=torch.float) * -(math.log(10000.0) / size))
+                              )
         pe[:, 0::2] = torch.sin(position.float() * div_term)
         pe[:, 1::2] = torch.cos(position.float() * div_term)
         pe = pe.unsqueeze(0)  # shape: [1, size, max_len]
