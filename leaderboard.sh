aa1f4da2bf1060bec265c2375dd0fa8ad1e3c041
Marvin! <koss@cl.uni-heidelberg.de>
2020-09-12 16:41:14 +0200

    cluster cfgs

---
+++ leaderboard.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+models_path="models/"
+models=$(ls $models_path)
+for model in $models; do
+	if [ -e "$models_path$model/best.ckpt" ]
+	then
+		path=$models_path$model
+		best_step=$(ls -l $path/best.ckpt | grep -o "\w*" | tail -n 2 | head -n 1)
+		# echo $model "best:" $best_step
+		validstxt=$path/validations.txt
+		best_bleu=$(grep "Steps: $best_step\s" $validstxt | awk '{print $8}')
+		echo $best_bleu $model $best_step
+	fi
+done
+	
