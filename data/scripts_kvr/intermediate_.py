8c9c345619f5a951436b3cd2bb3e3d857e438cb3
marvosyntactical <koss@cl.uni-heidelberg.de>
2020-09-19 12:17:37 +0200

    more linking progress, TODO tackle weather and multiple subjects

--- data/scripts_kvr/intermediate_.py
+++ data/scripts_kvr/intermediate_.py
@@ -12,17 +12,29 @@ SEP_CHAR = "@DOT"
 JOIN_SUBJ_CHAR = "_"
 CANON_START_CHAR = "@"
 
-
 def med(a, b):
     # calc minimum edit distance between strings a, b
     m, n = len(a), len(b)
-    distances = np.full((m,n), float("inf"))
+    d = np.zeros(m,n)
+    
+    for i in range(m):
+        d[i,0] = i
+    for j in range(n):
+        d[0,j] = j
+    for j in range(n):
+        for i in range(m):
+            if a[i] == b[j]:
+                substitutionCost = 0
+            else:
+                substitutionCost = 1
+            d[i,j] = min(d[i-1,j]+1, # del
+                        d[i,j-1]+1, # ins
+                        d[i-1,j-1]+substitutionCost # sub
+            )
+    return d
 
-    distance = 0
-    return distance
 
-
-def highest_med_match(query, keys):
+def lowest_med_match(query, keys):
     query = query.lower()
     keys = [key.lower() for key in keys]
     scores = [med(query,key) for key in keys]
@@ -32,8 +44,8 @@ def highest_med_match(query, keys):
     # sorts ascending
     sort_by_scores = sorted(list(zip(scores,keys)), key=lambda score_and_key: score_and_key[0])
 
-    best = sort_by_scores[0][1]
-    return best
+    best_idx = sort_by_scores[0][1]
+    return best_idx
 
 
 def find_subjs_in_seq(
@@ -45,8 +57,7 @@ def find_subjs_in_seq(
     ### START THIS HELPER FUNCTION FROM HERE ??
 
     entity_indices_local = set() 
-    rels_vals = OrderedDict() # {"poi_name": {5: ["dish", "parking"], i: ["surface", "entity"]}, "poi_type": ...}
-    prev_y = None
+    rels_vals = dict() # {"poi_name": {5: ["dish", "parking"], i: ["surface", "entity"]}, "poi_type": ...}
     # x == raw indices; y == mapped indices
     for x, y in enumerate(indices):
         raw = raw_seq[x]
@@ -55,15 +66,9 @@ def find_subjs_in_seq(
             entity_indices_local.add(y)
             if map_result not in rels_vals.keys():
                 rels_vals[map_result] = dict()
-                rels_vals[map_result][y] = [raw]
-            else: # entry already exists 
-                if y == prev_y: # multi word expression, e.g. 'the' '11th' => '@date'
-                    rels_vals[map_result][y] += [raw]
-                else: # multiple occurrences of e.g. @date 
-                    rels_vals[map_result] = dict()
-                    rels_vals[map_result][y] = [raw]
-        prev_y = y
-    
+            if y not in rels_vals[map_result].keys():
+                rels_vals[map_result][y] = []
+            rels_vals[map_result][y] += [raw]
     # start heuristic: go through these entities and find subjects for them
     
     domains = dict() # contains for each entity the domain if its subject or None
@@ -80,13 +85,10 @@ def find_subjs_in_seq(
 
 
 
-
-
 def intermediate( 
             raw_seqs: List[str], 
             can_seqs: List[str], 
             indices: List[int], 
-            matches: List[Tuple[str,List[str]]], 
             seq_idx: int, 
             subjs: dict, 
             ents: dict,
@@ -117,7 +119,6 @@ def intermediate(
     :param trg: bool whether to look at seq_idx'th trg sequence or, if False, at seq_idx'th source seq of batch
     """
 
-    del matches # TODO correct implementation of matches! dont use them until that :[]
     if not isinstance(subjs, defaultdict):
         assert type(subjs) == dict, type(subjs)
         subjs = defaultdict(lambda: None,subjs)
@@ -137,24 +138,40 @@ def intermediate(
             seqs_raw_separated[-1] += [tok]
     seqs_separated_start_indices += [len(raw_seqs)]
 
+
+    
+    global_can_seqs_bin = dict()# index this to get num of sequence or hit a None mine if u index at sep_char u noob lmao
+    global_offsets = dict() # start offsets of canonical sequences
+    rels_vals_per_seq = dict() # dict of seq idx : rels_vals dict for all visited sequences
+    subject_mapping = dict() # this should be set at end of while loop; otherwise no subject appeared in entire batch
+
+    # procedure: look at sequences in the order seq_idx[trg], seq_idx[src], seq_idx-1[trg],seq_idx-1[src],...,0[src]; then ascending afterwards
+    direction = -1 # start while loop in descending order, then ascend after hitting first src
     cache_trg = trg
-
-    subject_mapping = None # this should be set at end of while loop, otherwise no subject was found
-
-    # procedure: look at sequences in the order seq_idx[trg], seq_idx[subj], seq_idx-1[trg],seq_idx-1[subj],...,0; then ascending afterwards
-    look_at_seq = seq_idx
-    while look_at_seq < int(len(raw_seqs)//2):
-
-        # try to find subjects and handle cases
-        # if definite subject found, break
+    seq_offset = (seq_idx*2)+int(cache_trg)
+    while seq_offset < len(raw_seqs):
+        look_at_seq = (seq_offset//2)
+        # input((seq_idx, seq_offset, look_at_seq))
 
         raw_seq = seqs_raw_separated[int(trg)::2][look_at_seq]
-        raw_seq_start_idx = seqs_separated_start_indices[(look_at_seq*2)+int(trg)]
-        raw_seq_end_idx = seqs_separated_start_indices[(look_at_seq*2)+int(trg)+1]-2
+        raw_seq_start_idx = seqs_separated_start_indices[seq_offset]
+        raw_seq_end_idx = seqs_separated_start_indices[seq_offset+1]-2 # leave out delimiting “@DOT” sep_char
 
-        can_seq = can_seqs[indices[raw_seq_start_idx]:indices[raw_seq_end_idx]+1]
+        try:
+            can_seq = can_seqs[indices[raw_seq_start_idx]:indices[raw_seq_end_idx]+1]
+        except IndexError as IE:
 
-        local_indices = [idx - raw_seq_start_idx for idx in indices[raw_seq_start_idx:raw_seq_end_idx+1]]
+            print(can_seqs)
+            print(indices)
+            print(len(indices))
+            print(raw_seq_start_idx)
+            print(raw_seq_end_idx)
+            print(indices[raw_seq_start_idx])
+            print(indices[raw_seq_end_idx])
+
+        local_indices = [idx - indices[raw_seq_start_idx] for idx in indices[raw_seq_start_idx:raw_seq_end_idx+1]]
+        assert local_indices[0] == 0, (can_seq, indices[raw_seq_start_idx:raw_seq_end_idx+1], raw_seq_start_idx, raw_seq_end_idx)
+
 
         # start procedure: try to find subject indices in this sequence
         entity_indices_local, domains, domains_vals, rels_vals, subj_indices_local = find_subjs_in_seq(
@@ -164,6 +181,13 @@ def intermediate(
             subjs=subjs
         )
 
+        # cache vars for all visited sequences:
+        global_offsets[seq_offset] = indices[raw_seq_start_idx]
+        rels_vals_per_seq[seq_offset] = rels_vals
+        for i in range(indices[raw_seq_start_idx], indices[raw_seq_end_idx+1]):
+            global_can_seqs_bin[i] = seq_offset
+        
+
         # cache vars for the sequence of interest (first one)
         if trg == cache_trg and look_at_seq == seq_idx:
             can_seq_of_interest = can_seq
@@ -171,14 +195,18 @@ def intermediate(
             rels_vals_of_interest = rels_vals
 
         # heuristic switch case
-        # every case needs to set subj to the index of the found subject in the current can_seq
+        # every case needs to set subject_mapping to dict of entity_idx: subj_idx for all entities in the sent
         # in case of success and break
+
+        if len(domains_vals) == 0:
+            # sentence contains no entities
+            break
     
-        if domains_vals == {None}:
+        elif domains_vals == {None}:
 
             # TODO confirm subjs are in proper format
             # case 0: there is 0      subjects: extend search to other sequences in batch
-            input(("extend search ! No subjects found:", can_seq, raw_seqs, subjs))
+            input(("extend search ! No subjects found in (seq, then batch): ", can_seq, raw_seqs, subjs, look_at_seq, cache_trg, direction))
 
             # what order to recurse to other sentences in? probably backward, then forward
             # TODO this method of looking to other sequences in batch as backup is only better if
@@ -199,10 +227,10 @@ def intermediate(
             assert False, ("subjects of different domains found:", domains, can_seq, raw_seq)
 
         elif len(subj_indices_local) == 1:
-            # case 2: there is 1      subjects: take it for all attributes and break
-            subject_mapping = {ent: subj_indices_local[0] for ent in entity_indices_local}
+            # case 2: there is 1 subject: take it for all attributes and break
+            subject_mapping.update({ent: global_offsets[seq_offset]+subj_indices_local[0] for ent in entity_indices_local_of_interest})
 
-            print(f"found exactly one subject {can_seq[subj_indices_local[0]]} for sequence ", can_seq, raw_seq)
+            print(f"found exactly one subject {rels_vals[can_seq[subj_indices_local[0]]][subj_indices_local[0]]} for sequence ", can_seq, raw_seq)
 
             # unit test
             subj_canon = can_seq[subj_indices_local[0]] 
@@ -211,7 +239,7 @@ def intermediate(
             break # found subj; have set it and can stop searching
 
         else:
-            assert len(subj_indices_local) >= 1, domains
+            assert len(subj_indices_local) > 1, (domains,can_seq)
             print(f"found multiple subject contenders")
             # case 3: there is more   subjects: heuristics:
             #                   traffic: match POI attributes based on entities dict # what about distance, traffic info
@@ -223,8 +251,8 @@ def intermediate(
             if domain == "calendar":
                 assert False, f"found multiple events: {[can_seq[subj] for subj in subj_indices_local]} in {can_seq}"
             elif domain == "weather":
-
                 # TODO run some kind of dependency parse to match attributes with subjects
+                print(("\n"*4)+("\n"*4)+"WEATHER DOMAIN OMG WHATWEDO"+"\n"*4)
                 input((can_seq, can_seq_of_interest))
             else:
                 assert domain == "traffic"
@@ -241,11 +269,10 @@ def intermediate(
 
                 # look up poi info for each subject
 
-                subject_mapping = dict()
                 compare_subjects = dict()
 
                 for subj in subj_indices_local:
-                    subject_mapping[subj] = subj # set subject mapping to self
+                    subject_mapping[subj] = global_offsets[seq_offset]+subj # set subject mapping to self FIXME set value to global subject
                     can_subj = can_seq[subj]
 
                     subj_raw_list = rels_vals[can_subj][subj] # TODO should probably unit test if this is in ents.values()
@@ -259,46 +286,97 @@ def intermediate(
                 for entity in entity_indices_local_of_interest:
                     can_ent = can_seq[entity]
                     if "address" in can_ent: 
-                        address_raw_list = rels_vals[can_ent][entity]
+                        address_raw_list = rels_vals_per_seq[seq_idx][can_ent][entity]
                         address = " ".join(address_raw_list)
-                        proper_address_idx = highest_med_match(address, poi_address_list)
+                        proper_address_idx = lowest_med_match(address, poi_address_list)
                         subj = pois_by_address[poi_address_list[proper_address_idx]]
-                        subject_mapping[entity] = subj # hooray
-                    
+                        subject_mapping[entity] = global_offsets[seq_offset]+subj # hooray
                         
-                        
-            # first do descending from seq of interest; when hit 0 go back
-            direction = -1 if look_at_seq <= seq_idx and not (cache_trg == False and trg == True) else +1
-            increment = not trg if direction == -1 else trg
-            if increment:
-                look_at_seq += direction # first from src sequence to prev sequence, then afterwards
-                if look_at_seq < 0: # hit rock bottom; now continue with entries afterward
-                    if cache_trg: # started with target sequence, immediately go up one
-                        look_at_seq = seq_idx + 1
-                    else: # started with source sequence; "go up" by just switching trg below
-                        look_at_seq = seq_idx
-
-            # invert trg (alternate between looking at src and trg)
-            trg = not trg
+        # first do descending from seq of interest; when hit 0 go back
+        if seq_offset < 0: 
+            seq_offset = seq_idx 
+            direction *= -1 # start ascending
+            if cache_trg == True: # switch one extra time if we started with target because now we goin from src to src once
+                trg = not trg
+        seq_offset += direction # first from src sequence to prev sequence, then afterwards if seq_offset <= 0 and not trg: # hit first source; now continue with entries afterward
+        # inverttrg (alternate between looking at src and trg)
+        trg = not trg
         
 
     # TODO FIXME at end of while loop, 
-    # subj should be the index of the subject in the sequence of interest
-    # and can_seq, rels_vals, etc should be set by the last processed sequence that also returned subj
+    # subject_mapping should be entity: subject dict with  
+    # entity: index of entity in local can_seq
+    # subject: index of subject in global can_seqs
 
-    assert subject_mapping is not None, (can_seqs, can_seq_of_interest)
+    
+    # (can_seq, rels_vals, etc should be set to the last processed sequence that also returned subject_mapping)
+
+    assert subject_mapping != {}, (can_seqs, can_seq_of_interest, global_offsets, seq_offset, global_can_seqs_bin)
 
     subject_prefixes = dict()
-    for ent, subj in subject_mapping.items():
-        subj_canon = can_seq[subj] 
-        subj_raw_list = rels_vals[subj_canon][subj] # TODO should probably unit test if this is in ents.values()
+    for local_ent, global_subj in subject_mapping.items():
+
+        # FIXME TODO get these variables
+        subj_seq = global_can_seqs_bin[global_subj] # index in can_seqs NOTE probably look at seq but just figure out using sep in beginning
+        if subj_seq is None: # just gonna let this slide loll
+            subj_seq = global_can_seqs_bin[global_subj+1]
+
+        try:
+            subj = global_subj-global_offsets[subj_seq] # index in its local sequence
+        except KeyError as KE:
+            # XXX removeme
+            print(can_seq) # subj comes from this list anyways as this was last assigned
+            print(global_can_seqs_bin[global_subj])
+            print(global_can_seqs_bin[global_subj+1])
+            print(global_can_seqs_bin[global_subj+2])
+            print(global_can_seqs_bin[global_subj+3])
+            print(global_can_seqs_bin[global_subj+4])
+            print(can_seqs[global_subj])
+            print(can_seqs)
+            print(global_subj)
+            print(global_offsets)
+            print(global_can_seqs_bin)
+            print(subj_seq)
+            print(global_offsets[subj_seq])
+
+
+        subj_canon = can_seqs[global_subj] # poi_type
+        
+        try:
+            subj_raw_list = rels_vals_per_seq[subj_seq][subj_canon][subj] # TODO should probably unit test if this is in ents.values()
+        except KeyError as KE:
+            # XXX removeme
+            print(KE)
+            print(can_seq)
+            print(subj_seq)
+            print(subj_canon)
+            print(rels_vals_per_seq[subj_seq])
+            print(rels_vals_per_seq[subj_seq][subj_canon])
+
+        # input((subj_raw_list, rels_vals[subj_canon], subj, subject_mapping, can_seq))
+
         at_subj_raw_joined_ = CANON_START_CHAR + join_char.join(subj_raw_list) + join_char # @dish_parking_
-        subject_prefixes[ent] = at_subj_raw_joined_ 
+        subject_prefixes[local_ent] = at_subj_raw_joined_ 
 
-    intermediate_entities = {entity_idx_local: subject_prefixes[entity_idx_local] + can_seq_of_interest[entity_idx_local][1:]\
-            for entity_idx_local in entity_indices_local_of_interest}
+    intermediate_entities = dict()
+    for e_i in entity_indices_local_of_interest:
+        try:
+            subject_prefix = subject_prefixes[e_i]
+        except KeyError as KE:
+            # XXX removeme
+            print(subject_prefixes)
+            print(entity_indices_local_of_interest)
+            print(KE)
+            print(e_i)
+            assert False, subject_prefixes[e_i]
+        can_without_at = can_seq_of_interest[e_i][1:]
+        intermediate_label_i = subject_prefix + can_without_at
+        intermediate_entities[e_i] = intermediate_label_i
 
-    intermediate_canonized = [can if i not in entity_indices_local else intermediate_entities[i] for i, can in enumerate(can_seq_of_interest)]
+    intermediate_entities = {i: subject_prefixes[i] + can_seq_of_interest[i][1:] \
+            for i in entity_indices_local_of_interest}
+
+    intermediate_canonized = [can if i not in entity_indices_local_of_interest else intermediate_entities[i] for i, can in enumerate(can_seq_of_interest)]
 
     input(("canonized ",can_seq_of_interest, " to ", intermediate_canonized))
 
@@ -419,19 +497,57 @@ def main(args):
 
     # at each index in this list is the corresponding src & trg last for that batch
 
-
-    src_tgt_last = [(tok_X[lkp_last[i]],tok_Y[lkp_last[i]]) for i in range(len(tok_X))]
-
     if lower:
         sep_char = SEP_CHAR.lower()
     else:
         sep_char = SEP_CHAR
 
+    src_tgt_last = [(tok_X[lkp_last[i]],tok_Y[lkp_last[i]]) for i in range(len(tok_X))]
+
     src_tgt_last_join = [x + [sep_char] + y for x,y in src_tgt_last]
 
-    # canonize_sequence -> canon_seqs, indices, matches
-    context = [(raw_seqs,) + canonize_sequence(raw_seqs, efficient_entities) + (lkp_batch_idx[i],) for i, raw_seqs in enumerate(src_tgt_last_join)]
-    subj_indices = [intermediate(*tup, subjs=subjects, ents=entities, sep_char=sep_char) for tup in context]
+
+    class Batch_Iter:
+        def __init__(self):
+            pass
+
+        def __iter__(self, sequences=src_tgt_last_join, lkp_last=lkp_last):
+
+            i = 0
+            while i < len(sequences):
+                first = i
+                last = lkp_last[i]
+                seq_idx = lkp_batch_idx[i]
+                batch = []
+
+                while i <= last:
+                    batch += sequences[i]
+                    i+=1
+
+                # canonize_sequence -> canon_seqs, indices, matches
+                raw_seqs = sequences[last]
+                can_seqs, indices, matches = canonize_sequence(raw_seqs, efficient_entities)
+                del matches
+                input(("can seqs:", can_seqs))
+                input(("indices:", indices))
+                intermediately_canonized_batch = [
+                    intermediate(raw_seqs, can_seqs, indices, j, \
+                    subjs=subjects, ents=entities, sep_char=sep_char) \
+                     for j in range(last-first)]
+
+                yield intermediately_canonized_batch
+
+                i = last+1
+
+    batch_iter = Batch_Iter()
+
+    debug_raw_last_seqs = []
+    for raw_seqs in src_tgt_last_join:
+        if not debug_raw_last_seqs or debug_raw_last_seqs[-1] != raw_seqs:
+            debug_raw_last_seqs += [raw_seqs]
+    
+    for i, (raw_batch, processed_batch) in enumerate(zip(debug_raw_last_seqs, batch_iter)):
+        input(f"processed_batch:\n\n{processed_batch}\n\n\nAll correct?\n\n\n\n{debug_raw_last_seqs[i]}\n\n\n\n Continue ?\n\n\n")
     
     return 0
 
