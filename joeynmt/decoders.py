fix attention masking using torch.where: zero tensor now created without autograd, mask shape corrected

joeynmt/decoders.py
@@ -1635,7 +1635,11 @@ class Generator(Gen):
 
         if self.add_kb_biases_to_output and kb_values is not None and kb_probs is not None:
 
-            _batch, _unroll, _kb = kb_probs.shape
+            try:
+                _batch, _unroll, _kb = kb_probs.shape
+            except Exception as e:
+                print(kb_probs.shape)
+                raise e
 
             # kb_values: b x kb => b x time x kb
             kb_values = kb_values.unsqueeze(1).repeat((1, _unroll, 1))
@@ -1785,8 +1789,16 @@ def kvr_att_step(  utils_dims_cache, kb_feed_hidden_cache, query,
 
     if kb_mask is not None:
         # mask entries that arent actually assigned (happens in scheduling)
+        if len(kb_mask.shape)+1 == len(u_t_k.shape):
+            kb_mask = kb_mask.unsqueeze(1)
+
+        assert kb_mask.shape == u_t_k.shape, (kb_mask.shape, u_t_k.shape)
+
+        with torch.no_grad():
+            zeros_like_u_t_k = u_t_k.new_zeros(u_t_k.shape)
+
         u_t_k = torch.where(
-            ~kb_mask.unsqueeze(1), u_t_k, u_t_k.new_zeros(u_t_k.shape) 
+            ~kb_mask, u_t_k, zeros_like_u_t_k 
         )
 
     return u_t_k, utils_dims_cache, kb_feed_hidden_cache

joeynmt/search.py
@@ -455,8 +455,12 @@ def beam_search(
             kb_feed_hidden_cache=kb_feed_hidden_cache
         )
 
-        # generator applies output layer, biases towards KB values, then applies log_softmax
-        log_probs = generator(att_vectors, kb_values=kb_values, kb_probs=kb_scores)
+        try:
+            # generator applies output layer, biases towards KB values, then applies log_softmax
+            log_probs = generator(att_vectors, kb_values=kb_values, kb_probs=kb_scores)
+        except Exception as e:
+            print(kb_scores.shape)
+            raise e
 
         # hidden = ?? x batch*k x dec hidden        #FIXME why 3 ??????
         # att_scores = batch*k x 1 x src_len        #TODO  Find correct beam in dim 0 at every timestep.
@@ -503,7 +507,6 @@ def beam_search(
             + beam_offset[:topk_beam_index.size(0)].unsqueeze(1))
         select_indices = batch_index.view(-1) # batch * k 
 
-
         # append latest prediction
         alive_seq = torch.cat(
             [alive_seq.index_select(0, select_indices), # index first dim (batch * k) with the beams we want to continue this step
@@ -538,9 +541,6 @@ def beam_search(
                     kb_scores.transpose(1,2).index_select(0,select_indices)
                 ],
             -1) 
-        
-
-
 
         # which batches are finished? 
         is_finished = topk_ids.eq(eos_index) # batch x k